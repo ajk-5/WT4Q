c30804fae7bce61a898601106181a52f700fef4b
ajk-5 <139111881+ajk-5@users.noreply.github.com>
2025-08-01 23:51:30 +0200

    Adjust header layout for mobile

--- WT4Q/src/components/UserMenu.tsx
+++ WT4Q/src/components/UserMenu.tsx
@@ -47,8 +47,7 @@ export default function UserMenu() {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
-          width="20"
-          height="20"
+          className={styles.loginIcon}
           aria-hidden="true"
           focusable="false"
         >
@@ -57,7 +56,7 @@ export default function UserMenu() {
             d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 5v3h20v-3c0-3.3-6.7-5-10-5z"
           />
         </svg>
-        <span>Sign In</span>
+        <span className={styles.loginText}>Sign In</span>
       </Link>
     );
   }
