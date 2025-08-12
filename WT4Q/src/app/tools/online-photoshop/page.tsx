ca98f843ae3b93f210dc23653573a76504c4e43d
ajk-5 <139111881+ajk-5@users.noreply.github.com>
2025-08-12 15:00:12 +0200

    Remove dynamic import from online photoshop page

--- WT4Q/src/app/tools/online-photoshop/page.tsx
+++ WT4Q/src/app/tools/online-photoshop/page.tsx
@@ -1,12 +1,7 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import type { Metadata } from 'next';
-import dynamic from 'next/dynamic';
-
-const EditorShell = dynamic(
-  () => import('@/app/tools/online-photoshop/components/EditorShell'),
-  { ssr: false }
-);
+import EditorShell from '@/app/tools/online-photoshop/components/EditorShell';
 
 export const metadata: Metadata = {
   title: 'Free Online Photoshop',
