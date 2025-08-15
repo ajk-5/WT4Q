91b2f520b9d128e73a461f3fef6cb28410efcafd
karki <karki@et.intechinfo.fr>
2025-08-15 22:20:15 +0200

    conflict

--- Northeast/DTOs/ArticleRecommendationDto.cs
+++ Northeast/DTOs/ArticleRecommendationDto.cs
@@ -10,10 +10,5 @@ namespace Northeast.DTOs
         public Category Category { get; set; }
         public ArticleType ArticleType { get; set; }
     }
-<<<<<<< HEAD
-
 }
 
-=======
-}
->>>>>>> e3d7d77ac9e16aca8c916bf1e7e136100de0f21f
