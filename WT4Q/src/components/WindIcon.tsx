6ed144326d43e2a2a564a82f440e399db3743e95
ajk-5 <139111881+ajk-5@users.noreply.github.com>
2025-08-25 13:46:07 +0200

    feat: replace wind icon

--- WT4Q/src/components/WindIcon.tsx
+++ WT4Q/src/components/WindIcon.tsx
@@ -3,22 +3,17 @@ export default function WindIcon({ className }: { className?: string }) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={className}
       aria-hidden="true"
     >
-      <path
-        d="M3 12h10a3 3 0 1 0 0-6"
-        stroke="currentColor"
-        strokeWidth="2"
-        fill="none"
-      />
-      <path
-        d="M4 18h11a2 2 0 1 0 0-4"
-        stroke="currentColor"
-        strokeWidth="2"
-        fill="none"
-      />
-      <line x1="3" y1="6" x2="14" y2="6" stroke="currentColor" strokeWidth="2" />
+      <path d="M12.8 19.6A2 2 0 1 0 14 16H2" />
+      <path d="M17.5 8a2.5 2.5 0 1 1 2 4H2" />
+      <path d="M9.8 4.4A2 2 0 1 1 11 8H2" />
     </svg>
   );
 }
