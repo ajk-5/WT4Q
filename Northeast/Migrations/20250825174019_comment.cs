141487c4fa2d05b76f7b1717fcad552bf2b03cac
karki <karki@et.intechinfo.fr>
2025-08-25 19:41:28 +0200

    major countries added

---
+++ Northeast/Migrations/20250825174019_comment.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Northeast.Migrations
+{
+    /// <inheritdoc />
+    public partial class comment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Likes_UserId",
+                table: "Likes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_UserId_ArticleId",
+                table: "Likes",
+                columns: new[] { "UserId", "ArticleId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Likes_UserId_ArticleId",
+                table: "Likes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_UserId",
+                table: "Likes",
+                column: "UserId");
+        }
+    }
+}
