4bedb2ba956218ff41264f158b8a4fea92a53b62
karki <karki@et.intechinfo.fr>
2025-08-27 20:18:43 +0200

    index now

--- Northeast/Interface/IIndexNowService.cs
+++ Northeast/Interface/IIndexNowService.cs
@@ -2,11 +2,9 @@
 {
     public interface IIndexNowService
     {
-        public interface IIndexNowService
-        {
             string BuildArticleUrl(string slug);
             Task SubmitArticleAsync(string slug, CancellationToken ct = default);
             Task SubmitAsync(IEnumerable<string> urls, CancellationToken ct = default);
         }
     }
-}
+
